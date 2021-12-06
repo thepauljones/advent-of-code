1904d5df68e67a60b0bddd90dc2b1204b92b2e7f
Paul Jones <thepauljones@gmail.com>
2021-12-06 08:55:45 +0100

    Listen to me, in ANY other situation I'd clean this up, but it was SO fucking irritatingly written, and easy to FUCK UP that I ACTUALLY can't be bothered to clean up this GARBAGE right now, because I'm just glad the motherfucker is done.
    I will make it nice later but I'm too bitter at the moment

--- day3/day3.py
+++ day3/day3.py
@@ -5,7 +5,7 @@ counter = []
 for i in range(0, len(data[0])):
     counter.append(0)
 
-def part_one(data):
+def get_readings(data):
     for line in data:
         i = 0
         for value in [value for value in line]:
@@ -22,10 +22,59 @@ def part_one(data):
         gam[i] = 0 if counter[i] > len(data) / 2 else 1
         epi[i] = 1 if counter[i] > len(data) / 2 else 0
 
-    binary_result_gam = ''.join(str(b) for b in gam)
-    binary_result_epi = ''.join(str(b) for b in epi)
+    return [gam, epi]
 
-    return int(binary_result_gam, 2) * int(binary_result_epi, 2)
+readings = get_readings(data)
 
-print(part_one(data))
+def multiply_readings(readings):
+    return int(''.join(str(b) for b in  readings[0]), 2) * int(''.join(str(b) for b in readings[1]), 2)
+
+def part_one(readings):
+    return multiply_readings(readings)
+
+def strip(passed_data, mode):
+    for i in range(0, len(list(passed_data[0]))):
+        count = 0
+        ones = 0
+        zeroes = 0
+        for line in passed_data:
+            if (int(line[i]) == 1):
+                count = count + 1
+                ones = ones + 1
+            else:
+                zeroes = zeroes + 1
+
+        if (mode == 'most'):
+            if (ones > zeroes):
+                mask = 1
+            elif (ones == zeroes):
+                mask = 1
+            else:
+                mask = 0
+
+        elif (mode == 'fewest'):
+            if (ones > zeroes):
+                mask = 0
+            elif (zeroes == ones):
+                mask = 0
+            else:
+                mask = 1
+
+        new_data = []
+
+        for line in passed_data:
+            if (int(line[i]) == mask):
+                new_data.append(line)
+
+        passed_data = new_data[:]
+
+        if (len(passed_data) == 1):
+            return passed_data
+    return passed_data
+
+o2_reading = strip(data[:], 'most')
+Co2_reading = strip(data[:], 'fewest')
+
+print(multiply_readings([o2_reading, Co2_reading]))
+
 exit()
