3c7e4cafb16e00eba461615ef50e9f9ae812c3b0
Paul Jones <thepauljones@gmail.com>
2021-12-07 17:33:04 +0100

    Haha BRUTE force, don't care LOL

--- day7/day7.py
+++ day7/day7.py
@@ -1,3 +1,4 @@
+import math
 with open('data.dat') as file:
     raw_data = file.read().split(',')
 
@@ -5,10 +6,18 @@ pos = list(map(int, map(str.strip, raw_data)))
 
 all_diffs = []
 
+def calculate_cost(pos):
+    cost = 0
+    steps = 0
+    while(steps <= pos):
+        cost += steps
+        steps += 1
+
+    return cost
+
 for i in range(0, len(pos)):
-    diffs = [abs(x - i) for x in pos]
+    diffs = [calculate_cost(abs(x - i)) for x in pos]
     all_diffs.append((sum(diffs), i))
 
 all_diffs.sort(key=lambda x: x[0])
-
-print(all_diffs[0][0])
+print(all_diffs[0])
