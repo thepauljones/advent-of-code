3042c9a8e432687cdd1376b14dc92bcca4023ec1
Paul Jones <thepauljones@gmail.com>
2021-12-09 21:53:41 +0100

    Recur(recur(recur(recur(recur(sion)sion)sion)sion)sion)

--- day9/day9.py
+++ day9/day9.py
@@ -17,12 +17,80 @@ def get_adjacent(j, i, data):
     
     return adjs
 
-print(get_adjacent(0, 1, data))
+def get_non_9_adjacent_points(j, i, data):
+    points = []
+    if (j < len(data) - 1):
+        if data[j+1][i] != '9':
+            points.append((j+1, i))
 
-low_points = []
-for j in range(0, len(data)):
-    for i in range(0, len(data[0])):
-        if data[j][i] < min(get_adjacent(j, i, data)):
-            low_points.append(data[j][i])
+    if (j >= 1):
+        if data[j-1][i] != '9':
+            points.append((j - 1, i))
 
-print(sum(map(int, low_points))+len(low_points))
+    if (i < len(data[j]) - 1):
+        if data[j][i+1] != '9':
+            points.append((j, i + 1))
+
+    if (i >= 1):
+        if data[j][i-1] != '9':
+            points.append((j, i - 1))
+    
+    return points
+
+def contains_point(points, point):
+    if points == None:
+        return False
+
+    for p in points:
+        if p == point:
+            return True
+
+    return False
+
+def fill_basin(basin, data):
+    all_new_points = []
+    for point in basin:
+        new_points = get_non_9_adjacent_points(point[0], point[1], data)
+        for point in new_points:
+            if not contains_point(basin, point):
+                basin.append(point)
+                all_new_points.append(point)
+    
+    if len(all_new_points) == 0:
+        return basin
+    else:
+        return fill_basin(basin, data)
+            
+def part_two():
+    basins = []
+    current_basin = []
+    for j in range(0, len(data)):
+        for i in range(0, len(data[0])):
+            if (data[j][i] == '9'):
+                continue
+
+            point_safe = True
+            for basin in basins:
+                if contains_point(basin, (j, i)):
+                    point_safe = False
+                    break
+
+            if point_safe:
+                current_basin = fill_basin([(j, i)], data)
+                basins.append(current_basin)
+
+
+    sorted_basins = sorted(basins, key=len, reverse=True)
+    return len(sorted_basins[0]) * len(sorted_basins[1] * len(sorted_basins[2]))
+
+def part_one():
+    low_points = []
+    for j in range(0, len(data)):
+        for i in range(0, len(data[0])):
+            if data[j][i] < min(get_adjacent(j, i, data)):
+                low_points.append(data[j][i])
+
+    return sum(map(int, low_points))+len(low_points)
+
+print(part_one())
+print(part_two())
