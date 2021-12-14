a28ad1ec734ab27d552675a2ed9e7331250c2876
Paul Jones <thepauljones@gmail.com>
2021-12-14 21:30:07 +0100

    I also only really wrote half of this, total of about what, five minutes?

--- 2015/day2/day2.py
+++ 2015/day2/day2.py
@@ -7,5 +7,17 @@ for line in data:
     sides = [l * w, w * h, h * l]
     total += 2*sum(sides) + min(sides)
 
+# Part 1
 print(total)
+
+def calculate_ribbon():
+    total = 0
+    for line in data:
+        l, w, h = [int(x) for x in line.split('x')]
+        dims = sorted([w, h, l])
+        total += 2*dims[0] + 2*dims[1] + l*w*h
+
+    print(total)
+
+calculate_ribbon()
 
