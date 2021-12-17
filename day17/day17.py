91d509c1db9e6003ea7da9ab2d8ada1c5dcbc7c1
Paul Jones <thepauljones@gmail.com>
2021-12-17 22:52:09 +0100

    Shameless poison, not sorry :bowtie:

--- day17/day17.py
+++ day17/day17.py
@@ -7,22 +7,22 @@ target_x1, target_x2 = map(int, raw_x.split('=')[1].split('..'))
 target_y1, target_y2 = map(int, raw_y.split('=')[1].split('..'))
 
 def probe_in_target(pos):
-    return pos[0] >= target_x1 and pos[0] <= target_x2 and pos[1] >=target_y1 and pos[1] <= target_y2
+    result = pos[0] >= target_x1 and pos[0] <= target_x2 and pos[1] >= target_y1 and pos[1] <= target_y2
+    return result
 
 def probe_beyond_target(pos):
-    return pos[0] > target_x2 or pos[1] < target_y2
+    return pos[0] > target_x2 or pos[1] < target_y1
 
 Trajectories = {}
-
-def fire_probe(pos, vel, max_y = 0):
+def fire_probe(pos, vel):
     if probe_in_target(pos):
-        print('Bullseye')
-        return max_y
+        return True
 
+    if probe_beyond_target(pos):
+        return False
 
     posX = pos[0] + vel[0]
     posY = pos[1] + vel[1]
-    max_y = max(max_y, posY)
 
     if vel[0] > 0:
         velX = vel[0] - 1
@@ -35,22 +35,36 @@ def fire_probe(pos, vel, max_y = 0):
 
     velY = vel[1] - 1
 
-    Trajectories[vel] = posY
-
-    if probe_beyond_target(pos):
-        del Trajectories[vel]
-        return
-
-    return fire_probe((posX, posY), (velX, velY), max_y)
+    return fire_probe((posX, posY), (velX, velY))
 
 def part_one():
     pos = (0, 0)
-    max_y = 0
-    for x in range(1, 300):
-        for y in range(1, 300):
-            Trajectories[(x, y)] = 0
-            max_y = max(max_y, fire_probe(pos, (x, y)))
+    for x in range(-300, 300):
+        for y in range(-300, 300):
+            if fire_probe(pos, (x, y)):
+                Trajectories[(x, y)] = True
 
-    print(max_y)
+# test_cases = [(23,-10),  (25,-9),   (27,-5),   (29,-6),   (22,-6),   (21,-7),   (9,0),     (27,-7),   (24,-5),
+# (25,-7),   (26,-6),   (25,-5),   (6,8),     (11,-2),   (20,-5),   (29,-10),  (6,3),     (28,-7),
+# (8,0),     (30,-6),   (29,-8),   (20,-10),  (6,7),     (6,4),     (6,1),     (14,-4),   (21,-6),
+# (26,-10),  (7,-1),    (7,7),     (8,-1),    (21,-9),   (6,2),     (20,-7),   (30,-10),  (14,-3),
+# (20,-8),   (13,-2),   (7,3),     (28,-8),   (29,-9),   (15,-3),   (22,-5),   (26,-8),   (25,-8),
+# (25,-6),   (15,-4),   (9,-2),    (15,-2),   (12,-2),   (28,-9),   (12,-3),   (24,-6),   (23,-7),
+# (25,-10),  (7,8),     (11,-3),   (26,-7),   (7,1),     (23,-9),   (6,0),     (22,-10),  (27,-6),
+# (8,1),     (22,-8),   (13,-4),   (7,6),     (28,-6),   (11,-4),   (12,-4),   (26,-9),   (7,4),
+# (24,-10),  (23,-8),   (30,-8),   (7,0),     (9,-1),    (10,-1),   (26,-5),   (22,-9),   (6,5),
+# (7,5),     (23,-6),   (28,-10),  (10,-2),   (11,-1),   (20,-9),   (14,-2),   (29,-7),   (13,-3),
+# (23,-5),   (24,-8),   (27,-9),   (30,-7),   (28,-5),   (21,-10),  (7,9),     (6,6),     (21,-5),
+# (27, -10), (7, 2), (30, -9), (21, -8), (22, -7), (24, -9), (20, -6), (6, 9), (29, -5),
+# (8,-2),    (27,-8),   (30,-5),   (24,-7)]
+# 
+# 
+# for case in test_cases:
+#     result = fire_probe((0, 0), case)
+#     if not result:
+#         print('HUH? ', case)
+
+    print(Trajectories)
+    print(len(Trajectories))
 
 part_one()
