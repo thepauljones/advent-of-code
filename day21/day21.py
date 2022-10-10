8584bd7dd9e15458644e58701bde83671199991c
Paul Jones <thepauljones@gmail.com>
2022-10-10 20:00:51 +0200

    Day 21, part 1

--- day21/day21.py
+++ day21/day21.py
@@ -1,40 +1,56 @@
-with open('data.dat') as file:
-    [pone, ptwo] = [line.strip() for line in file]
-    
-
-one = pone[len(pone)-1:len(pone)]
-two = ptwo[len(ptwo)-1:len(ptwo)]
-
 num_rolls = 3
 
-score_one = 0
-score_two = 0
+scores = [0, 0]
 
-def play_game(round_num = 0):
+d = list(range(1, 101))
+print(d[0], d[99])
+
+def play_game(pos, round_num = 1):
     global score_one, score_two
+
     
     rolls = 0
     while rolls < num_rolls:
        nums  = roll_dice(round_num, True)
        rolls += 1
 
-    total = sum(nums)
+    player = int(round_num % 2 != 0)
 
-    if (round_num % 2 == 0):
-        score_one += total
-    else:
-        score_two += total
+    moves = sum(nums)
 
+    pos[player] += moves
 
-    if score_one > 999 or score_two > 999:
-        print(min([score_one, score_two]), round_num)
-        print(min([score_one, score_two]) * round_num)
+    if (pos[player] != 10):
+        pos[player] = pos[player] % 10
+
+    if(pos[player] == 0):
+        pos[player] = 10
+
+    score = pos[player]
+    scores[player] += score
+
+    if (scores[player] > 999):
+        print(round_num + 3)
+        final_num_rolls = round_num + 3
+        lowest_score = min(scores)
+        print(final_num_rolls, lowest_score)
+        print(final_num_rolls * lowest_score)
         exit()
+ 
+    print('Player', player + 1, 'rolls', '+'.join(map(str, nums)), 'and moves to space', score, 'for a total score of', scores[player]);
 
-    play_game(round_num + 3)
+    play_game(pos, round_num + 3)
 
 def roll_dice(round, deterministic = False):
+    res = []
     if (deterministic):
-        return [round * 3, round * 3 + 1, round * 3 + 2]
+        for x in range(3):
+            i = round + x
+            if (i > len(d) - 1):
+                i = i % len(d)
 
-play_game()
+            res.append(d[i])
+
+        return res
+
+play_game([6, 9], 0)
