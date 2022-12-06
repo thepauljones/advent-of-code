7adb8466fe217e0544add6badbe3faf39870f2cd
Paul Jones <thepauljones@gmail.com>
2022-12-06 20:10:21 +0100

    (R)ead (T)he (F)riendly (M)anual

--- 2016/day1/day1.py
+++ 2016/day1/day1.py
@@ -1,9 +1,11 @@
-i = 'R1, L4, L5, L5, R2, R2, L1, L1, R2, L3, R4, R3, R2, L4, L2, R5, L1, R5, L5, L2, L3, L1, R1, R4, R5, L3, R2, L4, L5, R1, R2, L3, R3, L3, L1, L2, R5, R4, R5, L5, R1, L190, L3, L3, R3, R4, R47, L3, R5, R79, R5, R3, R1, L4, L3, L2, R194, L2, R1, L2, L2, R4, L5, L5, R1, R1, L1, L3, L2, R5, L3, L3, R4, R1, R5, L4, R3, R1, L1, L2, R4, R1, L2, R4, R4, L5, R3, L5, L3, R1, R1, L3, L1, L1, L3, L4, L1, L2, R1, L5, L3, R2, L5, L3, R5, R3, L4, L2, R2, R4, R4, L4, R5, L1, L3, R3, R4, R4, L5, R4, R2, L3, R4, R2, R1, R2, L4, L2, R2, L5, L5, L3, R5, L5, L1, R4, L1, R1, L1, R4, L5, L3, R4, R1, L3, R4, R1, L3, L1, R1, R2, L4, L2, R1, L5, L4, L5'.split(', ')
+i = 'R8, R4, R4, R8'.split(', ')
 
 pos = (0, 0)
 direction = 0
 
 dirs = ['North', 'East', 'South', 'West']
+
+trail = []
 
 for step in i:
     turn = step[0:1]
@@ -21,24 +23,11 @@ for step in i:
     if direction == -1:
         direction = 3
 
-    # North
-    if direction == 0:
-        pos = (pos[0], pos[1] + dist)
+    walk(direction, dist)
 
-    # East
-    if direction == 1:
-        pos = (pos[0] + dist, pos[1])
+def walk(direction, dist):
+    steps = 0
+    while steps < dist:
 
-    # South
-    if direction == 2:
-        pos = (pos[0], pos[1] - dist)
+        steps += 1
 
-    # West
-    if direction == 3:
-        pos = (pos[0] - dist, pos[1])
-
-    print('Walked', dist, 'steps ', dirs[direction], 'to:', pos)
-
-
-print(pos)
-print(abs(pos[0]) + abs(pos[1]))
