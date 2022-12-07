435c784851d66556c80fc184b18a93caaad7bb40
Paul Jones <thepauljones@gmail.com>
2022-12-07 11:44:46 +0100

    getting further up the 🌴

--- 2022/day7/01.py
+++ 2022/day7/01.py
@@ -2,7 +2,7 @@ from pathlib import Path
 from treelib import Node, Tree
 
 script_location = Path(__file__).absolute().parent
-file_location = script_location / 'test-data.dat'
+file_location = script_location / 'data.dat'
 file = file_location.open()
 
 data = file.read().splitlines()
@@ -13,7 +13,7 @@ class File(object):
 
 
 tree = Tree()
-tree.create_node("/", "/")
+tree.create_node("/", "/", data=File(0))
 pwd = '/'
 
 def parse_command(command):
@@ -22,22 +22,22 @@ def parse_command(command):
 
     if elems[0] == '$':
         if elems [1] == 'cd':
-            pwd = tree.parent(pwd).tag if elems[2] == ".." else elems[2]
+            pwd = tree.parent(pwd).identifier if elems[2] == ".." else pwd + '/' + elems[2]
 
         if elems [1] == 'ls':
             return
     else:
         if elems[0] == 'dir':
-            tree.create_node(elems[1], elems[1], parent=pwd)
+            tree.create_node(elems[1], pwd + '/' + elems[1], parent=pwd, data=File(0))
         else:
-            tree.create_node(elems[1], elems[1], parent=pwd, data=File(elems[0]))
+            tree.create_node(elems[1], pwd + '/' + elems[1], parent=pwd, data=File(elems[0]))
 
 
 def solve(data):
     for command in data:
         parse_command(command)
 
-    tree.show()
+    tree.show(idhidden=False, data_property='size')
 
 solve(data)
 
