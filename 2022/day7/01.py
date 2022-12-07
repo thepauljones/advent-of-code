d3bc90cc765ac61c4c4d74f294edec3c2793df91
Paul Jones <thepauljones@gmail.com>
2022-12-07 16:14:09 +0100

    🏝️

--- 2022/day7/01.py
+++ 2022/day7/01.py
@@ -7,13 +7,20 @@ file = file_location.open()
 
 data = file.read().splitlines()
 
+script_location = Path(__file__).absolute().parent
+file_location = script_location / 'test-data.dat'
+file = file_location.open()
+
+test_data = file.read().splitlines()
+
 class File(object):
-        def __init__(self, size):
+        def __init__(self, size, element_type):
             self.size = size
+            self.element_type = element_type
 
 
 tree = Tree()
-tree.create_node("/", "/", data=File(0))
+tree.create_node("/", "/", data=File(0, 'dir'))
 pwd = '/'
 
 def parse_command(command):
@@ -28,19 +35,33 @@ def parse_command(command):
             return
     else:
         if elems[0] == 'dir':
-            tree.create_node(elems[1], pwd + '/' + elems[1], parent=pwd, data=File(0))
+            tree.create_node(elems[1], pwd + '/' + elems[1], parent=pwd, data=File(0, 'dir'))
         else:
-            tree.create_node(elems[1], pwd + '/' + elems[1], parent=pwd, data=File(elems[0]))
+            tree.create_node(elems[1], pwd + '/' + elems[1], parent=pwd, data=File(elems[0], 'file'))
+
+def getSize(dirNode, size = 0):
+    for child in tree.children(dirNode.identifier):
+        if child.data.element_type == 'dir':
+            size += getSize(child, size)
+        else:
+            size += int(child.data.size)
+
+    return size
 
 
 def solve(data):
     for command in data:
         parse_command(command)
 
-    tree.show(idhidden=False, data_property='size')
+    tree.show()
 
+    sizes = []
+    for directory in tree.filter_nodes(lambda x: x.data.element_type == 'dir'):
+        size = getSize(directory)
+
+        if size <= 100000:
+            sizes.append(size)
+        
+    print(sum(sizes))
+    
 solve(data)
-
-def test():
-    assert solve() == 0
-
