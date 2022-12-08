3d482c04cd122fa88a0a9d70388b63cf8ce916fa
Paul Jones <thepauljones@gmail.com>
2022-12-08 11:30:01 +0100

    this _should_ work, right?

--- 2022/day8/01.py
+++ 2022/day8/01.py
@@ -14,23 +14,69 @@ test_data = list(map(list, file.read().splitlines()))
 
 
 def solve(grid):
-
     # Create a dictionary of positions all set to invisible
     visible = {}
+
+    visibility = []
+
+    columns = []
     for i in range(0, len(grid)):
+        col = []
         for j in range(0, len(grid[i])):
+            col.append(int(grid[j][i]))
+        columns.append(col)
+
+    for i in range(0, len(grid)):
+        visibility.append([])
+        for j in range(0, len(grid[i])):
+            height = int(grid[i][j])
+
             key = ''.join([str(i), str(j)])
             visible[key] = False
 
-            if i == 0 or i == len(grid) - 1 or j == 0 or j == len(grid[0]) - 1:
+            toLeft = list(map(int, grid[i][0:j]))
+            toRight = list(map(int, grid[i][j + 1:]))
+
+            above = list(map(int, columns[j][0:i]))
+            below = list(map(int, columns[j][i + 1:]))
+
+            # visible from the left
+            if height > max(toLeft, default=-1):
                 visible[key] = True
+
+            # visible from the bottom
+            if height > max(below, default=-1):
+                visible[key] = True
+
+            # visible from the right
+            if height > max(toRight, default=-1):
+                visible[key] = True
+
+            # visible from the top
+            if height > max(above, default=-1):
+                visible[key] = True
+
+            visibility[i].append('O' if visible[key] == True else 'X')
+
+            print('-----------------')
+            print('Key', key, height)
+            print('Left', toLeft)
+            print('Right', toRight)
+            print('Above', above)
+            print('Below', below)
+            print('-----------------')
 
 
     result = list(visible.values()).count(True)
     print(result)
+
+    for line in visibility:
+        print(''.join(line))
+
     return result
 
 solve(test_data)
+#solve(data)
 
 
 def test():
