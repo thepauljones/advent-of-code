814a32362a907ff49d84c55d309bbfeb57661d18
Paul Jones <thepauljones@gmail.com>
2022-12-14 19:33:25 +0100

    rendering the whole shebang in pyglet

--- 2022/day14/01.py
+++ 2022/day14/01.py
@@ -1,8 +1,16 @@
 from pathlib import Path
 from copy import deepcopy
+import pyglet
+from pyglet.window import Window
+from pyglet.gl import *
+from pyglet import shapes
+
+batch = pyglet.graphics.Batch()
+window = pyglet.window.Window(width=1024, height=768)
+pyglet.gl.glClearColor(0.5,0,0,1)
 
 script_location = Path(__file__).absolute().parent
-file_location = script_location / 'data.dat'
+file_location = script_location / 'test-data.dat'
 file = file_location.open()
 
 data = file.read().splitlines()
@@ -14,19 +22,16 @@ for i in range(510):
         row.append('.')
     cave.append(row)
 
-
 def draw_line(a, b):
-    print(a, b)
     x0, y0 = a
     x1, y1 = b
-
+    
     if x0 == x1:
         for y in range(min(y0, y1), max(y0, y1) + 1):
-            print('printing # ', x0, y)
             cave[y][x0] = '#'
+
     elif y0 == y1:
         for x in range(min(x0, x1), max(x0, x1) + 1):
-            print('printing # ', x, y0)
             cave[y0][x] = '#'
 
 
@@ -35,13 +40,18 @@ for line in data:
     for i in range(len(points) - 1):
         draw_line([int(x) for x in points[i].split(',')], [int(x) for x in points[i + 1].split(',')])
 
-def render_cave(cave):
-    for y in range(0, 150):
-        for x in range(390, 504):
-            print(cave[y][x], end='')
-        print('\n')
 
-render_cave(cave)
+@window.event
+def on_draw():
+    lines = []
+    for y in range(len(cave)):
+        for x in range(len(cave[y])):
+            if cave[y][x] == '#':
+                lines.append(shapes.Rectangle(x=x, y=550-y, width=1, height=1, color=(0, 244, 0), batch=batch))
+    
+    batch.draw()
+
+pyglet.app.run()
 
 def solve():
     return 0
