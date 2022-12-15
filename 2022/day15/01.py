d0fb893909724df5f985e72564081b31c14ea419
Paul Jones <thepauljones@gmail.com>
2022-12-15 11:11:23 +0100

    started

---
+++ 2022/day15/01.py
@@ -0,0 +1,50 @@
+from pathlib import Path
+
+script_location = Path(__file__).absolute().parent
+file_location = script_location / 'test-data.dat'
+file = file_location.open()
+
+data = file.read().splitlines()
+
+beaconMap = {}
+
+originX = 0
+originY = 0
+height = 0
+width = 0
+
+for line in data:
+    s, b = line.split(':')
+    sensor = (int(s[s.index('x'):].split(',')[0].split('=')[1]),int(s[s.index('y'):].split(',')[0].split('=')[1]))
+    beacon = (int(b[b.index('x'):].split(',')[0].split('=')[1]),int(b[b.index('y'):].split(',')[0].split('=')[1]))
+
+    # Set the bounds of the array
+    originX = min(sensor[0], beacon[0], originX)
+    originY = min(sensor[1], beacon[1], originY)
+
+    # Set the bounds of the array
+    width = max(sensor[0], beacon[0], width)
+    height = max(sensor[1], beacon[1], height)
+
+    beaconMap[sensor] = beacon
+
+
+def drawMap():
+    for y in range(originY, height):
+        for x in range(originX, width):
+            if (x,y) in beaconMap:
+                print('X', end='')
+            else:
+                print('.', end='')
+        print('\n')
+
+
+drawMap()
+
+
+def solve():
+    return 0
+
+
+def test():
+    assert solve() == 0
