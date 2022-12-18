224cb1ce0e62007c6ac72ede64dd896c12f89864
Paul Jones <thepauljones@gmail.com>
2022-12-18 23:06:47 +0100

    (day 1)

---
+++ 2019/day1/02-Tyranny-of-rocket.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+import math
+
+script_location = Path(__file__).absolute().parent
+file_location = script_location / 'data.dat'
+file = file_location.open()
+
+def calculate(mass):
+    result = math.floor(mass / 3 - 2)
+    return result
+
+masses = [int(x) for x in file.readlines()]
+
+total = 0
+for mass in masses:
+    fuel = calculate(mass)
+    extra = fuel
+    extras = []
+    while extra > 0:
+        extra = calculate(extra)
+
+        if extra > 0:
+            extras.append(extra)
+
+    total += fuel + sum(extras)
+
+print(total)
