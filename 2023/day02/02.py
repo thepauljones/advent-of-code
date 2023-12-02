bb00581c4bff084be0821b3299bdc9c4e129aae8
Paul Jones <thepauljones@gmail.com>
2023-12-02 10:22:17 +0100

    Oft expectation fails, and most oft there where most it promises

--- 2023/day02/02.py
+++ 2023/day02/02.py
@@ -1,8 +1,5 @@
 from pathlib import Path
 import re
-import sys
-from termcolor import colored, cprint
-from threading import Timer
 
 script_location = Path(__file__).absolute().parent
 file_location = script_location / "data.dat"
@@ -12,5 +9,31 @@ data = file.read().splitlines()
 
 
 def solve():
+    answer = 0
+    for line in data:
+        tot = {"blue": [], "red": [], "green": []}
+        game, handfuls = line.split(":")
+        gameId = int(game.split(" ")[1])
+
+        cubes = handfuls.split(";")
+
+        for colors in cubes:
+            handfuls = [x.strip() for x in colors.split(", ")]
+            for individual in handfuls:
+                num, col = individual.split(" ")
+                tot[col].append(int(num))
+
+        print(tot)
+
+        pieces = []
+        for col in tot:
+            pieces.append(max(tot[col]))
+
+        power = pieces[0] * pieces[1] * pieces[2]
+        print(power)
+        answer += power
+
+    print("answer", answer)
+
 
 solve()
