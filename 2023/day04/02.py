f3ac1756da3ee326e703963b756a6be6a2684630
Paul Jones <thepauljones@gmail.com>
2023-12-04 08:05:55 +0100

    easypeasylemonsqueezy

---
+++ 2023/day04/02.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+
+script_location = Path(__file__).absolute().parent
+file_location = script_location / "test-data.dat"
+file = file_location.open()
+
+data = file.read().splitlines()
+
+
+def solve():
+    answer = 0
+    for line in data:
+        cardPoints = 0
+        gamePart, numberSets = line.split(":")
+        _, gameId = gamePart.split()
+        winning, scratchcard = [x.split() for x in numberSets.split("|")]
+
+        for x in winning:
+            if x in scratchcard:
+                print(x, "is winning")
+                if cardPoints == 0:
+                    cardPoints = 1
+                else:
+                    cardPoints *= 2
+
+        answer += cardPoints
+
+    print(answer)
+
+
+solve()
