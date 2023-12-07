6af268238b1748ecbde0603206d5b76fb46d2e37
Paul Jones <thepauljones@gmail.com>
2023-12-07 11:25:49 +0100

    For michel

--- 2023/day05/02.py
+++ 2023/day05/02.py
@@ -2,7 +2,7 @@ from pathlib import Path
 from collections import defaultdict
 
 script_location = Path(__file__).absolute().parent
-file_location = script_location / "data.dat"
+file_location = script_location / "input.txt"
 file = file_location.open()
 
 data = file.read().splitlines()
