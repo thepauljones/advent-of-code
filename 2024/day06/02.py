c307bd77d6eb42433df6c39d6a93e78dffab88ff
Paul Jones <paul.william.jones@maersk.com>
2024-12-06 14:54:34 +0100

    18 minutes in heaven and POP, outcomes the answer. The muscles on my CPU now....

--- 2024/day06/02.py
+++ 2024/day06/02.py
@@ -2,32 +2,114 @@ from pathlib import Path
 import re
 
 script_location = Path(__file__).absolute().parent
-file_location = script_location / "test-data.dat"
+file_location = script_location / "data.dat"
 file = file_location.open()
 
 data = file.readlines()
 
-left = []
-right = []
+orientations = {
+    '^': (-1, 0),
+    '>': (0, 1),
+    'v': (1, 0),
+    '<': (0, -1)
+}
 
+room = []
 
-answer = 0
+guard_pos = ('^', (0, 0))
+initial_guard_pos = guard_pos
 
+j = 0
 for line in data:
-    one, two = map(int, line.split())
-    left.append(one)
-    right.append(two)
+    rline = list(line.strip())
+    for o in orientations.keys():
+        if o in rline:
+            guard_pos = (o, (j, rline.index(o)))
+            initial_guard_pos = (o, (j, rline.index(o)))
+    room.append(rline)
+    j += 1
+
+num_obstacles_in_room = 0
+for line in room:
+    for pos in line:
+        if pos == "#":
+            num_obstacles_in_room += 1
 
 
-for item in left:
-    increment = item * right.count(item)
-    answer += increment
+def move(guard_pos, room):
+    dir, pos = guard_pos
+    next_position = (pos[0] + orientations[dir][0], pos[1] + orientations[dir][1])
+
+    # Walked out of room
+    if next_position[0] < 0 or next_position[0] > len(room) - 1 or next_position[1] < 0 or next_position[1] > len(room[0]) - 1:
+        return -1
+
+    # Walked into an empty space, no problem
+    if room[next_position[0]][next_position[1]] == '.' or room[next_position[0]][next_position[1]] == '^':
+        return (dir, next_position)
+
+    if room[next_position[0]][next_position[1]] == '#':
+        # get next orientation
+        return (turn(dir), pos)
+
+    print(guard_pos, 'STUCK')
+    exit()
 
 
 
-def solve():
+def turn(facing):
+    os = list(orientations.keys())
+    for i in range(len(os)):
+        if os[i] == facing:
+            if i + 1 > len(os) - 1:
+                return os[0]
+        else:
+            continue
 
-    print(answer)
+        return os[i + 1]
+
+assert turn('^') == '>'
+assert turn('>') == 'v'
+assert turn('v') == '<'
+assert turn('<') == '^'
+
+def test_room(room, walked):
+    while True:
+        guard_pos = move(walked[-1], room)
+
+        if guard_pos == -1:
+            return False
+
+        if guard_pos in walked:
+            return True
+
+        walked.append(guard_pos)
+
+def get_path(room, walked):
+    while True:
+        guard_pos = move(walked[-1], room)
+        if guard_pos == True:
+            break
+        if guard_pos == -1:
+            return walked
+        walked.append(guard_pos)
 
 
-solve()
+path = get_path(room, [initial_guard_pos])
+
+
+loops = []
+for step in path:
+    j, i = step[1]
+    if room[j][i] == "#" or room[j][i] == "^":
+        continue
+    temp = room[j][i]
+    room[j][i] = "#"
+    isLoop = test_room(room, [initial_guard_pos])
+    if isLoop:
+        loops.append((j, i))
+
+    room[j][i] = temp
+
+print(len(list(set(loops)))) # -1 because the initial guard position is also counted as a position which can't have an obstacle added
+
