5bba26d7cb4dfbcdf415a32f02e3c290e41151e9
Paul Jones <paul.william.jones@maersk.com>
2024-12-21 23:49:54 +0100

    recursion always wants you to return 1

--- 2024/day19/01.py
+++ 2024/day19/01.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from functools import cache
 
 script_location = Path(__file__).absolute().parent
 file_location = script_location / "data.dat"
@@ -10,20 +11,20 @@ t, d = data.split("\n\n")
 towels = list(reversed(sorted(list(map(str.strip, t.split(","))), key=len)))
 designs = d.strip().split("\n")
 
-print(towels)
 
-print(designs)
-
-
+@cache
 def test_design(design):
+    res = 0
     if len(design) == 0:
-        return True
+        return 1
 
-    for a in towels:
-        if design.startswith(a):
-            return test_design(design[len(a) :])
+    else:
+        res = 0
+        for a in towels:
+            if design.startswith(a):
+                res += test_design(design[len(a) :])
 
-    return False
+    return res
 
 
 ans = 0
